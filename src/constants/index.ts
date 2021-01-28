d456858eef2ff2dc49cc465b5aa7302c62faf18f
bastiengrignon <basgrignon@gmail.com>
2021-01-28 15:07:44 +0100

    ADD constants

---
+++ src/constants/index.ts
@@ -0,0 +1,13 @@
+export enum RouterUrl {
+    home = "/",
+    programmation = "/programmation",
+    association = "/association",
+    information = "/information"
+}
+
+export enum TabName {
+    festival = "Le festival",
+    programmation = "Programmation",
+    association = "L'association",
+    information = "Infos Pratiques"
+}
