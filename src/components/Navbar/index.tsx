2f9d5ccc7719e87c24959394a69daa85c395441b
bastiengrignon <basgrignon@gmail.com>
2021-01-28 15:33:22 +0100

    UPDATE Navbar with state of tab

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,13 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import {Link, useHistory} from "react-router-dom"
 import {RouterUrl, TabName} from "../../constants"
 import {BiMenuAltRight} from "react-icons/bi"
 
+const activeClass = "text-yellow-300 hover:font-normal"
+const inactiveClass = "text-white hover:text-yellow-300"
+
 const Navbar: React.FC = () => {
+    const history = useHistory()
     return (
         <div className="flex text-white">
             <img className="absolute top-0 w-full" src={`${process.env.PUBLIC_URL}/img/wave.svg`}
@@ -16,12 +20,25 @@ const Navbar: React.FC = () => {
                     festival
                 </div>
             </div>
-            <div className="fixed inline-flex items-center right-0 top-0 text-3xl font-light uppercase z-10 mt-10 mr-10 border border-red-600">
+            <div
+                className="fixed inline-flex items-center right-0 top-0 text-3xl font-light uppercase z-10 mt-10 mr-10">
                 <BiMenuAltRight className="transform rotate-180 text-6xl mx-2"/>
-                <Link to={RouterUrl.home} className="px-4 hover:text-yellow-300">{TabName.festival}</Link>
-                <Link to={RouterUrl.programmation} className="px-4 hover:text-yellow-300">{TabName.programmation}</Link>
-                <Link to={RouterUrl.association} className="px-4 hover:text-yellow-300">{TabName.association}</Link>
-                <Link to={RouterUrl.information} className="px-4 hover:text-yellow-300">{TabName.information}</Link>
+                <Link to={RouterUrl.home}
+                    className={`px-4 ${history.location.pathname === RouterUrl.home ? activeClass : inactiveClass}`}>
+                    {TabName.festival}
+                </Link>
+                <Link to={RouterUrl.programmation}
+                    className={`px-4 ${history.location.pathname === RouterUrl.programmation ? activeClass : inactiveClass}`}>
+                    {TabName.programmation}
+                </Link>
+                <Link to={RouterUrl.association}
+                    className={`px-4 ${history.location.pathname === RouterUrl.association ? activeClass : inactiveClass}`}>
+                    {TabName.association}
+                </Link>
+                <Link to={RouterUrl.information}
+                    className={`px-4 ${history.location.pathname === RouterUrl.information ? activeClass : inactiveClass}`}>
+                    {TabName.information}
+                </Link>
             </div>
         </div>
     )
