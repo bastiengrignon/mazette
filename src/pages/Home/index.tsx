1604d0fd4db66b395a2362f2517bebdb9dade45b
bastiengrignon <basgrignon@gmail.com>
2021-01-28 22:44:47 +0100

    UPDATE Home page with list of films

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,24 @@
 import React from "react"
 import Navbar from "../../components/Navbar"
+import Film from "../../components/Film"
+import {films} from "../../constants"
 
 const Home: React.FC = () => (
     <div className="flex flex-col">
         <Navbar/>
-        <div className="flex z-10 text-my-indigo mt-56">
-            <div className="ml-48 text-center text-5xl uppercase font-bold">
+        <div className="flex flex-col z-10 text-my-indigo mt-56">
+            <div className="text-center text-5xl uppercase font-bold w-1/2">
                 Les courts-métrages
+            </div>
+            <div className="mt-24 flex flex-row-reverse justify-evenly w-3/5 mx-auto">
+                {
+                    films.map((film, i) => (
+                        <Film key={i} filmName={film.filmName} author={film.author}
+                            description={film.description} date={film.date}
+                            location={film.location} duration={film.duration}
+                            imgThumbnail={film.imgThumbnail} imgExtended={film.imgExtended}/>
+                    ))
+                }
             </div>
         </div>
     </div>
