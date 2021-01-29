a27485b4e94d7dd86b6eed371ca07e5e9c97e581
bastiengrignon <basgrignon@gmail.com>
2021-01-29 03:00:40 +0100

    ADD z-index 0 to avoid overlap

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => (
     <div className="flex flex-col">
         <Navbar/>
         <div className="flex flex-col z-10 text-my-indigo mt-10 sm:mt-28 md:mt-56">
-            <div className="text-center text-2xl sm:text-5xl uppercase font-bold w-full xl:w-1/2">
+            <div className="text-center text-2xl sm:text-5xl uppercase font-bold w-full xl:w-1/2 z-0">
                 Les courts-métrages
             </div>
             <div className="mt-8 md:mt-16 lg:mt-20 xl:mt-24 flex flex-col sm:flex-row-reverse mx-auto justify-evenly flex-wrap lg:w-4/5 xl:w-3/5 ">
