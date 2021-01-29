64b9698960ace481563620e38bc519fe73d502f4
bastiengrignon <basgrignon@gmail.com>
2021-01-29 03:03:25 +0100

    RENAME menu variable

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -8,7 +8,7 @@ const inactiveClass = "text-white hover:text-yellow-300"
 
 const Navbar: React.FC = () => {
     const history = useHistory()
-    const [burgerOpen, setBurgerOpen] = useState<boolean>(false)
+    const [menuOpen, setMenuOpen] = useState<boolean>(false)
 
     return (
         <div className="flex text-white">
@@ -21,30 +21,30 @@ const Navbar: React.FC = () => {
                     indigo
                 </div>
                 <div
-                    className={`flex justify-end text-sm sm:text-2xl lg:text-3xl font-light sm:font-medium ${burgerOpen ? "mb-16 sm:mb-0" : "mb-0"}`}>
+                    className={`flex justify-end text-sm sm:text-2xl lg:text-3xl font-light sm:font-medium ${menuOpen ? "mb-16 sm:mb-0" : "mb-0"}`}>
                     festival
                 </div>
             </div>
             <div
-                className={`fixed lg:absolute flex flex-col lg:flex-row text-right items-end xl:items-center ${burgerOpen ? "bg-my-indigo" : ""} w-full lg:w-3/4 right-0 top-0 text-sm sm:text-base md:text-2xl xl:text-3xl font-light uppercase z-0 mt-0 mr-0 md:mt-2 lg:mt-5 xl:mt-10 lg:mr-2 xl:mr-10`}>
+                className={`fixed lg:absolute flex flex-col lg:flex-row text-right items-end xl:items-center ${menuOpen ? "bg-my-indigo" : ""} w-full lg:w-3/4 right-0 top-0 text-sm sm:text-base md:text-2xl xl:text-3xl font-light uppercase z-0 mt-0 mr-0 md:mt-2 lg:mt-5 xl:mt-10 lg:mr-2 xl:mr-10`}>
                 <BiMenuAltRight
                     className="transform lg:rotate-180 text-3xl rounded-md flex items-end md:text-5xl mx-2 block lg:hidden"
-                    onClick={() => setBurgerOpen(!burgerOpen)}/>
+                    onClick={() => setMenuOpen(!menuOpen)}/>
                 <Link to={RouterUrl.home}
-                    className={`px-4 py-1 ${burgerOpen ? "block" : "hidden"} lg:block w-full ${history.location.pathname === RouterUrl.home
+                    className={`px-4 py-1 ${menuOpen ? "block" : "hidden"} lg:block w-full ${history.location.pathname === RouterUrl.home
                         ? activeClass : inactiveClass}`}>
                     {TabName.festival}
                 </Link>
                 <Link to={RouterUrl.programmation}
-                    className={`px-4 py-1 ${burgerOpen ? "block" : "hidden"} lg:block w-full ${history.location.pathname === RouterUrl.programmation ? activeClass : inactiveClass}`}>
+                    className={`px-4 py-1 ${menuOpen ? "block" : "hidden"} lg:block w-full ${history.location.pathname === RouterUrl.programmation ? activeClass : inactiveClass}`}>
                     {TabName.programmation}
                 </Link>
                 <Link to={RouterUrl.association}
-                    className={`px-4 py-1 ${burgerOpen ? "block" : "hidden"} lg:block w-full ${history.location.pathname === RouterUrl.association ? activeClass : inactiveClass}`}>
+                    className={`px-4 py-1 ${menuOpen ? "block" : "hidden"} lg:block w-full ${history.location.pathname === RouterUrl.association ? activeClass : inactiveClass}`}>
                     {TabName.association}
                 </Link>
                 <Link to={RouterUrl.information}
-                    className={`px-4 py-1 ${burgerOpen ? "block" : "hidden"} lg:block w-full whitespace-nowrap mb-0 md:mb-4 lg:mb-0  ${history.location.pathname === RouterUrl.information ? activeClass : inactiveClass}`}>
+                    className={`px-4 py-1 ${menuOpen ? "block" : "hidden"} lg:block w-full whitespace-nowrap mb-0 md:mb-4 lg:mb-0  ${history.location.pathname === RouterUrl.information ? activeClass : inactiveClass}`}>
                     {TabName.information}
                 </Link>
             </div>
