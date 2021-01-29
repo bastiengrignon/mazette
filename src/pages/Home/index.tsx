4c94bbc0e4dc928d14baad58fd7c9105b9cb27dd
bastiengrignon <basgrignon@gmail.com>
2021-01-29 02:33:14 +0100

    UPDATE Home page to support mobile

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,11 +6,11 @@ import {films} from "../../constants"
 const Home: React.FC = () => (
     <div className="flex flex-col">
         <Navbar/>
-        <div className="flex flex-col z-10 text-my-indigo mt-56">
-            <div className="text-center text-5xl uppercase font-bold w-1/2">
+        <div className="flex flex-col z-10 text-my-indigo mt-10 sm:mt-28 md:mt-56">
+            <div className="text-center text-2xl sm:text-5xl uppercase font-bold w-full xl:w-1/2">
                 Les courts-métrages
             </div>
-            <div className="mt-24 flex flex-row-reverse justify-evenly w-3/5 mx-auto">
+            <div className="mt-8 md:mt-16 lg:mt-20 xl:mt-24 flex flex-col sm:flex-row-reverse mx-auto justify-evenly flex-wrap lg:w-4/5 xl:w-3/5 ">
                 {
                     films.map((film, i) => (
                         <Film key={i} filmName={film.filmName} author={film.author}
