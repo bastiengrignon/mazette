0685c77ece9cb9e0dc74f93ce8867c74b90a6c0d
bastiengrignon <basgrignon@gmail.com>
2021-02-16 16:27:50 +0100

    TRY to put the Footer at the bottom of the page

--- src/index.tsx
+++ src/index.tsx
@@ -10,16 +10,22 @@ import Association from "./pages/Association"
 import Information from "./pages/Information"
 import Home from "./pages/Home"
 import {RouterUrl} from "./constants"
+import Footer from "./components/Footer"
 
 const App: React.FC = () => (
-    <Router>
-        <Switch>
-            <Route path={RouterUrl.programmation} component={Programmation}/>
-            <Route path={RouterUrl.association} component={Association}/>
-            <Route path={RouterUrl.information} component={Information}/>
-            <Route path={RouterUrl.home} component={Home}/>
-        </Switch>
-    </Router>
+    <body className="flex flex-col h-full">
+        <Router>
+            <div style={{flex: "1 0 auto"}}>
+                <Switch>
+                    <Route path={RouterUrl.programmation} component={Programmation}/>
+                    <Route path={RouterUrl.association} component={Association}/>
+                    <Route path={RouterUrl.information} component={Information}/>
+                    <Route path={RouterUrl.home} component={Home}/>
+                </Switch>
+            </div>
+            <Footer/>
+        </Router>
+    </body>
 )
 
 ReactDOM.render(
