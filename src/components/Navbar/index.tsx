81d7bed6a9fd117e0d80d479ca0f3ea431db6dd3
bastiengrignon <basgrignon@gmail.com>
2021-02-16 01:23:19 +0100

    ADD little vertical margin to be more user-friendly on mobile

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -55,25 +55,26 @@ const Navbar: React.FC = () => {
             className="flex flex-col lg:flex-row justify-between lg:items-center text-white bg-my-indigo w-full p-4 mb-10">
             <Link to={RouterUrl.home} className="flex flex-col text-center text-xl uppercase">
                 Festival
-                <div className="text-4xl italic text-yellow-400 font-medium tracking-widest uppercase">
+                <div
+                    className="text-4xl italic text-yellow-400 font-medium tracking-widest uppercase">
                     Indigo
                 </div>
             </Link>
             <div className="flex flex-col items-end">
-                <BiMenu className="text-3xl md:text-5xl lg:hidden"
+                <BiMenu className="text-3xl md:text-5xl lg:hidden mb-2"
                     onClick={() => setMenuOpen(!menuOpen)}/>
                 <div
                     className={`${menuOpen ? "flex" : "hidden"} lg:flex flex-col items-end lg:flex-row lg:justify-end w-full uppercase text-base md:text-2xl xl:text-3xl`}>
                     <Link to={RouterUrl.home}
-                        className={`${menuOpen ? "block" : "hidden"} lg:block lg:mx-4 ${activeTab(RouterUrl.home)} pr-6 md:pr-8 lg:pr-0`}>
+                        className={`${menuOpen ? "block" : "hidden"} lg:block lg:mx-4 my-1 lg:my-0 ${activeTab(RouterUrl.home)}`}>
                         {TabName.festival}
                     </Link>
                     <DropDown name={TabName.programmation} items={programmationItems}
-                        className={`${menuOpen ? "block" : "hidden"} lg:block lg:mx-4 ${activeTab(RouterUrl.programmation)}`}/>
+                        className={`${menuOpen ? "block" : "hidden"} lg:block lg:mx-4 my-1 lg:my-0 ${activeTab(RouterUrl.programmation)}`}/>
                     <DropDown name={TabName.association} items={associationItems}
-                        className={`${menuOpen ? "block" : "hidden"} lg:block lg:mx-4 ${activeTab(RouterUrl.association)}`}/>
+                        className={`${menuOpen ? "block" : "hidden"} lg:block lg:mx-4 my-1 lg:my-0 ${activeTab(RouterUrl.association)}`}/>
                     <Link to={RouterUrl.information}
-                        className={`${menuOpen ? "block" : "hidden"} lg:block lg:mx-4 ${activeTab(RouterUrl.information)} pr-6 md:pr-8 lg:pr-0`}>
+                        className={`${menuOpen ? "block" : "hidden"} lg:block lg:mx-4 my-1 lg:my-0 ${activeTab(RouterUrl.information)}`}>
                         {TabName.information}
                     </Link>
                 </div>
