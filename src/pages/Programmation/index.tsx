2e75c72de7714b50326c2eb0c0897928d5f3cbe1
bastiengrignon <basgrignon@gmail.com>
2021-03-02 23:42:54 +0100

    ADD Musique introduction sentence

--- src/pages/Programmation/index.tsx
+++ src/pages/Programmation/index.tsx
@@ -22,9 +22,14 @@ const Programmation: React.FC = () => (
                 }
             </div>
             <div
-                className="text-left text-2xl sm:text-5xl uppercase font-bold w-full xl:w-1/2 my-52"
+                className="text-left text-2xl sm:text-5xl uppercase font-bold w-full xl:w-1/2 my-5"
                 id={programmationTitle.musique}>
                 {programmationTitle.musique}
+            </div>
+            <div className="ml-2 text-base sm:text-lg md:text-2xl">
+                Nous vous proposons une sélection de talents émergents de la scène musicale locale.
+                Jazz, électro, musique tzigane, un tourbillon de genres musicaux pour le plaisir de
+                vos oreilles !
             </div>
             <div
                 className="text-left text-2xl sm:text-5xl uppercase font-bold w-full xl:w-1/2 my-52"
