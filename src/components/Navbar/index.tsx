19689b67482f2b3630d3c201218da771990675c1
Perrault <lucas.perrault@hotmail.com>
2021-03-14 20:50:12 +0100

    FIX desktop display

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -8,6 +8,7 @@ import {
 } from "../../constants"
 import DropDown from "../DropDown"
 import {AiOutlineClose, AiOutlineMenu} from "react-icons/ai"
+import Wave from "../Wave"
 
 export const activeClass = "text-yellow-400"
 export const inactiveClass = "lg:hover:text-white"
@@ -16,45 +17,35 @@ const Navbar: React.FC = () => {
     const [menuOpen, setMenuOpen] = useState<boolean>(false)
 
     return (
-        <nav
-            className="flex flex-col lg:flex-row justify-start lg:justify-between text-my-indigo lg:items-center w-full h-96 p-4"
-            style={{
-                backgroundImage: `url(${process.env.PUBLIC_URL}/img/exported-wave.svg)`,
-                backgroundRepeat: "no-repeat",
-                width: "100%",
-            }}>
-            <Link to={RouterUrl.home} className="flex flex-col text-center text-xl uppercase">
-                Festival
-                <div
-                    className="text-4xl italic text-yellow-400 font-medium tracking-widest uppercase">
-                    Indigo
+        <aside className="relative h-96">
+            <Wave text="CINEMA ET CONCERTS EN PLEIN AIR"/>
+            <nav
+                className="absolute z-10 flex flex-col lg:flex-row justify-start lg:justify-between text-my-indigo lg:items-center w-full p-6">
+                <Link to={RouterUrl.home} className="flex flex-col text-center text-xl uppercase">
+                    Festival
+                    <div
+                        className="text-4xl italic text-yellow-400 font-medium tracking-widest uppercase">
+                        Indigo
+                    </div>
+                </Link>
+                <div className="flex flex-col items-end">
+                    <div className="text-3xl md:text-5xl lg:hidden mb-2" onClick={() => setMenuOpen(!menuOpen)}>
+                        {!menuOpen ? <AiOutlineMenu/> : <AiOutlineClose/>}
+                    </div>
+                    <div
+                        className={`${menuOpen ? "flex items-center" : "hidden"} lg:flex flex-col items-end lg:flex-row lg:justify-end w-full uppercase text-xl xl:text-3xl`}>
+                        <NavLink to={RouterUrl.home} activeClassName={activeClass} exact={true} onClick={() => setMenuOpen(false)} className={`${menuOpen ? "block" : "hidden"} lg:block lg:mx-4 my-4 lg:my-0 ${inactiveClass}`}>
+                            {TabName.festival}
+                        </NavLink>
+                        <DropDown name={TabName.programmation} items={programmationItems} onItemClick={() => setMenuOpen(false)} className={`${menuOpen ? "block" : "hidden"} lg:block lg:mx-4 my-4 lg:my-0 ${inactiveClass}`}/>
+                        <DropDown name={TabName.association} items={associationItems} onItemClick={() => setMenuOpen(false)} className={`${menuOpen ? "block" : "hidden"} lg:block lg:mx-4 my-4 lg:my-0 ${inactiveClass}`}/>
+                        <NavLink to={RouterUrl.information} activeClassName={activeClass} onClick={() => setMenuOpen(false)} className={`${menuOpen ? "block" : "hidden"} lg:block lg:mx-4 my-4 lg:my-0 ${inactiveClass}`}>
+                            {TabName.information}
+                        </NavLink>
+                    </div>
                 </div>
-            </Link>
-            <div className="flex flex-col items-end">
-                <div className="text-3xl md:text-5xl lg:hidden mb-2" onClick={() => setMenuOpen(!menuOpen)}>
-                    {!menuOpen ? <AiOutlineMenu/> : <AiOutlineClose/>}
-                </div>
-                <div
-                    className={`${menuOpen ? "flex items-center" : "hidden"} lg:flex flex-col items-end lg:flex-row lg:justify-end w-full uppercase text-2xl xl:text-3xl`}>
-                    <NavLink to={RouterUrl.home} activeClassName={activeClass} exact={true}
-                        onClick={() => setMenuOpen(false)}
-                        className={`${menuOpen ? "block" : "hidden"} lg:block lg:mx-4 my-4 lg:my-0 ${inactiveClass}`}>
-                        {TabName.festival}
-                    </NavLink>
-                    <DropDown name={TabName.programmation} items={programmationItems}
-                        onItemClick={() => setMenuOpen(false)}
-                        className={`${menuOpen ? "block" : "hidden"} lg:block lg:mx-4 my-4 lg:my-0 ${inactiveClass}`}/>
-                    <DropDown name={TabName.association} items={associationItems}
-                        onItemClick={() => setMenuOpen(false)}
-                        className={`${menuOpen ? "block" : "hidden"} lg:block lg:mx-4 my-4 lg:my-0 ${inactiveClass}`}/>
-                    <NavLink to={RouterUrl.information} activeClassName={activeClass}
-                        onClick={() => setMenuOpen(false)}
-                        className={`${menuOpen ? "block" : "hidden"} lg:block lg:mx-4 my-4 lg:my-0 ${inactiveClass}`}>
-                        {TabName.information}
-                    </NavLink>
-                </div>
-            </div>
-        </nav>
+            </nav>
+        </aside>
     )
 }
 
