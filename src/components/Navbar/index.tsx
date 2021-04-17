a17494a05fb0c6bf1e4bd5c71d939cfd173d9cb2
bastiengrignon <basgrignon@gmail.com>
2021-04-17 13:04:15 +0200

    :lipstick::iphone: Adjust colors & size for screen

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -14,23 +14,23 @@ const Navbar: React.FC = () => {
             <nav
                 className="absolute z-20 flex flex-row justify-start lg:justify-between text-logo-blue lg:items-center w-full p-2">
                 <Link id="logo" to={RouterUrl.home}
-                    className="md:text-xl uppercase z-20 w-1/4 ml-10">
+                    className="md:text-xl uppercase z-20 w-1/4 ml-2 sm:ml-5 md:ml-8 lg:ml-10">
                     <img src={`${process.env.PUBLIC_URL}/img/test_logo9.png`} alt="Logo"
                         className=""/>
                 </Link>
 
                 <span className="block lg:hidden w-full">
-                    <div className="text-2xl md:text-4xl absolute right-2 text-logo-yellow"
+                    <div className="text-2xl md:text-4xl absolute right-2 text-white"
                         onClick={() => setMenuOpen(!menuOpen)}>
                         {!menuOpen ? <AiOutlineMenu/> : <AiOutlineClose/>}
                     </div>
                     <NavbarTabs setMenuOpen={setMenuOpen} isMenuOpen={menuOpen}
-                        className="absolute w-full uppercase px-2 mt-5 top-8 left-0 md:top-12 text-3xl text-center bg-logo-blue"/>
+                        className="absolute w-full uppercase px-2 mt-5 top-8 left-0 md:top-12 text-3xl text-center bg-test-green"/>
                 </span>
 
                 <span className="hidden lg:block w-full flex-nowrap">
                     <NavbarTabs isMenuOpen={menuOpen} setMenuOpen={setMenuOpen}
-                        className="lg:uppercase lg:flex lg:flex-row lg:justify-end lg:text-3xl"/>
+                        className="lg:uppercase lg:flex lg:flex-row lg:justify-end lg:text-2xl xl:text-3xl 2xl:text-4xl"/>
                 </span>
 
             </nav>
