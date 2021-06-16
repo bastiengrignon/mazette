920bf0d57db8a4529ad9a499008059dc10bbf42d
bastiengrignon <basgrignon@gmail.com>
2021-06-16 22:50:51 +0200

    Update Informations text

--- src/pages/Information/index.tsx
+++ src/pages/Information/index.tsx
@@ -11,8 +11,9 @@ const Information: React.FC = () => (
                 {informationTitle.food}
             </div>
             <div className="px-2 mt-5 text-black text-xl">
-                Une buvette sera proposée sur le lieu du festival, avec bière, vin et jus de fruits,
-                ainsi que de la petite restauration (galettes saucisses).
+                Une buvette sera proposée sur le lieu du festival, dans le respect des règles sanitaires en vigueur.
+                <br/>
+                Bière, vin et jus de fruit locaux seront à la carte !
                 <br/>
                 Vous pourrez également profiter du bar-restaurant de La Payotte, situé à deux pas du
                 lieu du festival.
@@ -24,18 +25,23 @@ const Information: React.FC = () => (
                 <div className="w-full lg:w-1/2 px-2 my-5">
                     <Maps/>
                 </div>
-                <div className="text-black text-lg sm:text-xl mx-10">
+                <div className="text-black text-lg sm:text-xl mx-10 space-y-3">
                     <p><span className="font-extrabold">Adresse : </span>
-                        La Payotte, Gué de Mazé, 49630 Mazé-Milon
+                        Gué de Mazé, 49630 Mazé-Milon (en face de La Payotte)
                     </p>
-                    <p className="pt-5"><span className="font-extrabold">En voiture : </span>
-                        tourner à droite sur la D356, un parking sera prévu à l’entrée du festival
+                    <p><span className="font-extrabold">En voiture : </span>
+                        depuis Angers, en arrivant au premier rond-point de Mazé tourner à droite sur la D55 (direction
+                        Saint-Mathurin), un parking sera prévu à l’entrée du festival
+                    </p>
+                    <p>
+                        <span className="font-extrabold">En bus : </span>
+                        depuis Angers ligne 403, arrêt Mazé-Milon - Carrefour route de Fayet
                     </p>
                     <p><span className="font-extrabold">En vélo : </span>
-                        5 min du centre-ville de Mazé
+                        5 minutes depuis le centre-ville de Mazé
                     </p>
                     <p><span className="font-extrabold">En covoiturage : </span>
-                        utilisez Blablacar, pour une planète verte en bonne santé
+                        covoiturez avec Blablacar ou entre amis !
                     </p>
                 </div>
             </div>
