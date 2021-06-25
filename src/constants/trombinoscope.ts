e835ba48f2395c7fdc2573b9dacec7fb567615aa
bastiengrignon <basgrignon@gmail.com>
2021-06-25 22:03:28 +0200

    Add trombinoscope of the team

---
+++ src/constants/trombinoscope.ts
@@ -0,0 +1,28 @@
+import {staticImgFolder} from "./index"
+
+export const trombinoscope = [
+    {
+        src: `${staticImgFolder}/trombi_mathilde.jpg`,
+        alt: "Mathilde"
+    },
+    {
+        src: `${staticImgFolder}/trombi_constance.jpg`,
+        alt: "Constance"
+    },
+    {
+        src: `${staticImgFolder}/trombi_jeanne.jpg`,
+        alt: "Jeanne"
+    },
+    {
+        src: `${staticImgFolder}/trombi_marine.jpg`,
+        alt: "Marine"
+    },
+    {
+        src: `${staticImgFolder}/trombi_lisa.jpg`,
+        alt: "Lisa"
+    },
+    {
+        src: `${staticImgFolder}/trombi_lea.jpg`,
+        alt: "lea"
+    },
+]
