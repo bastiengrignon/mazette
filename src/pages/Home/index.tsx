bf5c3df03a2ed6687fa709f2ab4d11510c5acdcc
bastiengrignon <basgrignon@gmail.com>
2021-06-27 02:00:08 +0200

    :iphone: Re-design Home & Programmation pages

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,18 @@
 import React from "react"
 
 const Home: React.FC = () => (
-    <div className="flex flex-col sm:flex-row w-full mt-5 sm:mt-1 md:mt-5 lg:mt-10 page-content">
-        <div className="w-full sm:w-2/3">
-            <p className="text-base md:text-xl lg:text-2xl px-1 sm:px-4 py-4">
-                Du cinéma, de la musique et du plein air, le festival Mazette! c’est ce combo chaleureux et festif que
-                nous vous proposons de vivre les 30 et 31 juillet 2021, sur les bords de l’Authion à Mazé-Milon (49).
-                <br/>
-                Une programmation éclectique sur deux soirées pour découvrir le court-métrage sur grand écran et sous
-                toutes ses formes, de la fiction à l’animation, de la comédie au documentaire.
-                <br/>
-                Le festival Mazette! c’est aussi des concerts, avec une sélection de talents émergents de la scène
-                musicale locale. Electro pop, jazz ou encore musique de l’Est seront au rendez-vous, dans un décor
-                champêtre et estival !
-            </p>
-        </div>
-        <div className="w-full sm:w-1/3 flex justify-center items-center">
-            <img src={`${process.env.PUBLIC_URL}/img/branche_bleu_test.png`} alt="Branche Bleu" className="w-44 md:w-60 lg:w-64 2xl:w-72"/>
-        </div>
+    <div className="page-content">
+        <p className="text-base md:text-xl lg:text-2xl text-center mx-24">
+            Du cinéma, de la musique et du plein air, le festival Mazette! c’est ce combo chaleureux et festif que
+            nous vous proposons de vivre les 30 et 31 juillet 2021, sur les bords de l’Authion à Mazé-Milon (49).
+            <br/><br/>
+            Une programmation éclectique sur deux soirées pour découvrir le court-métrage sur grand écran et sous
+            toutes ses formes, de la fiction à l’animation, de la comédie au documentaire.
+            <br/>
+            Le festival Mazette! c’est aussi des concerts, avec une sélection de talents émergents de la scène
+            musicale locale. Electro pop, jazz ou encore musique de l’Est seront au rendez-vous, dans un décor
+            champêtre et estival !
+        </p>
     </div>
 )
 
--- src/pages/Programmation/index.tsx
+++ src/pages/Programmation/index.tsx
@@ -1,50 +1,54 @@
 import React from "react"
-import {programmationTitle} from "../../constants"
+import { programmationTitle } from "../../constants"
+import { musics } from "../../constants/music"
+import Vignette from "../../components/Vignette"
 
 export const titleCSS = "text-2xl sm:text-3xl uppercase font-bold text-test-red mt-12 mb-4"
 export const subtitleCSS = "text-base sm:text-lg md:text-xl"
 
-const dateCSS = "text-xl sm:text-2xl"
-const progCSS = "text-2xl sm:text-4xl font-bold my-5 text-center bg-test-green bg-opacity-5"
+const dateCSS = "text-xl sm:text-2xl mt-4"
+const progCSS = "text-2xl sm:text-4xl font-bold my-5 text-center bg-test-green bg-opacity-5 w-full"
 
 const Programmation: React.FC = () => (
     <div className="flex flex-col z-10 page-content">
         <div className={titleCSS} id={programmationTitle.musique}>{programmationTitle.musique}</div>
         <p className={subtitleCSS}>
-            Nous vous proposons une sélection de talents émergents de la scène musicale locale.
-            Jazz, électro, musique tzigane, un tourbillon de genres musicaux pour le plaisir de
-            vos oreilles !
+            Chaque soirée débutera à 19h par deux concerts, avec une sélection de talents émergents de la scène musicale
+            locale. Electro pop, jazz ou encore musique de l’Est seront au rendez-vous, dans un décor champêtre et
+            estival !
         </p>
-        <p className={progCSS}>
-            Programmation à venir...
+        <p className={dateCSS}>
+            Vendredi 30 juillet
         </p>
+        <div className="grid grid-cols-2 gap-1 sm:gap-10">
+            <Vignette type="music" properties={musics[0]} />
+            <Vignette type="music" properties={musics[1]} />
+        </div>
+        <p className={dateCSS}>
+            Samedi 31 juillet
+        </p>
+        <div className="grid grid-cols-2 gap-1 sm:gap-10">
+            <Vignette type="music" properties={musics[2]} />
+            <Vignette type="music" properties={musics[3]} />
+        </div>
 
         <div className={titleCSS} id={programmationTitle.films}>{programmationTitle.films}</div>
-        <div>
-            <p className={dateCSS}>
-                Vendredi 30 juillet
-            </p>
-            <p className={progCSS}>
-                On vous dévoile bientôt la programmation !
-            </p>
-            <p className={dateCSS}>
-                Samedi 31 juillet
-            </p>
-            <p className={progCSS}>
-                On vous dévoile bientôt la programmation !
-            </p>
+        <p className={dateCSS}>
+            Vendredi 30 juillet
+        </p>
+        <div className="grid grid-cols-1 gap-10">
+            {/*<Vignette type="movie" properties={films[0]} />*/}
+            {/*<Vignette type="movie" properties={films[1]} />*/}
+            <p className={progCSS}>Programmation à venir...</p>
         </div>
-        {/*            <div
-                className="mt-10 flex flex-wrap justify-evenly mx-auto">
-                {
-                    films.map((film, i) => (
-                        <Film key={i} filmName={film.filmName} author={film.author}
-                            description={film.description} date={film.date}
-                            location={film.location} duration={film.duration}
-                            imgThumbnail={film.imgThumbnail} imgExtended={film.imgExtended}/>
-                    ))
-                }
-            </div>*/}
+        <p className={dateCSS}>
+            Samedi 31 juillet
+        </p>
+        <div className="grid grid-cols-1 gap-10">
+            <p className={progCSS}>Programmation à venir...</p>
+            {/*<Vignette type="movie" properties={films[2]} />*/}
+            {/*<Vignette type="movie" properties={films[2]} />*/}
+        </div>
 
         <div className={titleCSS} id={programmationTitle.concours}>{programmationTitle.concours}</div>
         <p className={subtitleCSS}>
