8bd15e85f2c7db448a0da23ee13f1c5753050a96
bastiengrignon <basgrignon@gmail.com>
2021-06-27 12:03:06 +0200

    :boom: Implements React.lazy()

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,32 @@
-import React from "react"
+import React, { lazy, Suspense } from "react"
 import ReactDOM from "react-dom"
 import {
     BrowserRouter as Router,
     Switch,
     Route
 } from "react-router-dom"
-import Programmation from "./pages/Programmation"
-import Association from "./pages/Association"
-import Information from "./pages/Information"
-import Home from "./pages/Home"
-import {RouterUrl} from "./constants"
+const Programmation = lazy(() => import("./pages/Programmation"))
+const Association = lazy(() => import("./pages/Association"))
+const Information = lazy(() => import("./pages/Information"))
+const Home = lazy(() => import("./pages/Home"))
+import { RouterUrl } from "./constants"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 import "./index.css"
+import { FaCircleNotch } from "react-icons/fa"
 
 const App: React.FC = () => (
     <div className="min-h-full flex flex-col justify-between">
         <Router>
             <Navbar/>
-            <Switch>
-                <Route path={RouterUrl.programmation} component={Programmation}/>
-                <Route path={RouterUrl.association} component={Association}/>
-                <Route path={RouterUrl.information} component={Information}/>
-                <Route path={RouterUrl.home} component={Home}/>
-            </Switch>
+            <Suspense fallback={<FaCircleNotch className="animate-spin"/>}>
+                <Switch>
+                    <Route path={RouterUrl.programmation} component={Programmation}/>
+                    <Route path={RouterUrl.association} component={Association}/>
+                    <Route path={RouterUrl.information} component={Information}/>
+                    <Route path={RouterUrl.home} component={Home}/>
+                </Switch>
+            </Suspense>
         </Router>
         <Footer/>
     </div>
