bb80957d95bb012b3f6236788061f6f63310e135
bastiengrignon <basgrignon@gmail.com>
2021-06-27 10:20:51 +0200

    :sparkles: Redesign footer with partner's list

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,27 +1,31 @@
 import React from "react"
-import {FaFacebookF, FaInstagram} from "react-icons/fa"
+import { FaFacebookF, FaInstagram } from "react-icons/fa"
+import { partners } from "../../constants/partners"
+import { externalLinks } from "../../constants"
 
-const links = {
-    instagram: "https://www.instagram.com/festivalmazette",
-    facebook: "https://www.facebook.com/festivalmazette"
-}
 
 const Footer: React.FC = () => (
-    <footer className="text-white text-center w-full p-2 mt-10" style={{backgroundColor: "#55505c"}}>
-        <div className="flex justify-between">
-            <div className="flex">
-                logo des partenaires
+    <footer className="text-white text-center w-full p-2 mt-10 bg-footer-gray">
+        <div className="grid grid-cols-5 xl:grid-cols-6 grid-rows-4">
+            <div className="col-span-5 row-span-5 text-left flex flex-row justify-evenly overflow-auto">
+                {
+                    partners.map((partner, key) => (
+                        <img key={key} src={partner.src} alt={partner.alt} className="w-auto h-16  2xl:h-24 object-contain"/>
+                    ))
+                }
             </div>
-            <div className="flex justify-center text-3xl">
-                <a href={links.instagram} target={"_blank"} rel={"noopener noreferrer"}>
-                    <FaInstagram className="mx-2 hover:text-test-red"/>
+            <div className="col-span-1 row-span-4 flex justify-evenly items-center text-5xl">
+                <a href={externalLinks.social.instagram} target={"_blank"} rel={"noopener noreferrer"}>
+                    <FaInstagram className="mx-2 hover:text-test-green"/>
                 </a>
-                <a href={links.facebook} target={"_blank"} rel={"noopener noreferrer"}>
-                    <FaFacebookF className="mx-2 hover:text-test-red"/>
+                <a href={externalLinks.social.facebook} target={"_blank"} rel={"noopener noreferrer"}>
+                    <FaFacebookF className="mx-2 hover:text-test-green"/>
                 </a>
+            </div>
+            <div className="col-span-6 row-span-1 italic">
+                Mentions Légales /// crédits
             </div>
         </div>
-        <div className="italic">Mentions Légales /// crédits</div>
     </footer>
 )
 
