f8654e1e6cba1b004d1eeb20f58c4523e1c6b966
bastiengrignon <basgrignon@gmail.com>
2021-06-27 02:00:51 +0200

    :iphone: Use grid instead of flex

--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -21,30 +21,27 @@ const Popup: React.FC<PopupProps> = ({
     duration,
     img,
     open
-}) => (
-    <div
-        className={`fixed z-30 inset-0 overflow-auto ${open ? "block" : "hidden"} flex`}>
+}) => {
+    const isMusic = () => (date === "" && location === "" && duration === "")
+    return (
         <div
-            className="absolute inset-0 z-20 bg-gray-500 opacity-75 transition-opacity flex items-start justify-center"/>
-        <div
-            className="flex flex-col m-auto bg-white shadow-2xl mx-auto z-30 max-w-5xl border-4 border-test-green">
-            <div
-                className="flex justify-between items-start lg:items-center border-b p-2 mt-5 sm:mt-0 ml-5 text-xl md:text-5xl bg-white">
-                <div>
-                    <span className="font-medium uppercase">{filmName}</span>
-                    <span className="text-base md:text-4xl">&nbsp;{` de ${author}`}</span>
+            className={`fixed z-30 inset-0 overflow-auto ${open ? "block" : "hidden"} flex`}>
+            <div className="absolute inset-0 z-20 bg-gray-500 opacity-75 transition-opacity flex items-start justify-center"/>
+            <div className="grid grid-cols-6 grid-rows-6 w-11/12 sm:w-5/6 md:w-3/4 lg:w-2/3 xl:w-1/2 h-3/4 sm:h-1/2 shadow-2xl m-auto z-50 bg-white border-4 border-test-green p-1">
+                <div className="col-span-5 border-b border-gray-300 flex items-center">
+                    <span className="font-semibold uppercase text-xl sm:text-2xl lg:text-3xl">
+                        {filmName} { !isMusic() && <span className="text-base italic">de {author}</span> }
+                    </span>
                 </div>
-                <div className="flex cursor-pointer text-2xl sm:text-3xl hover:text-test-green">
-                    <AiFillCloseCircle/>
+                <div className="col-span-1 border-b border-gray-300 flex justify-end">
+                    <AiFillCloseCircle className="cursor-pointer text-2xl sm:text-3xl hover:text-test-green"/>
                 </div>
-            </div>
-            <div className="mx-5 mt-5 text-2xl">{date}, {location}, {duration}</div>
-            <div className="flex flex-col sm:flex-row m-5 items-start">
-                <img src={img} alt={img} className="w-56 lg:w-1/3 h-auto mx-auto rounded"/>
-                <div className="px-5 mt-4 sm:mt-0 text-sm sm:text-base md:text-lg lg:text-2xl leading-relaxed">{description}</div>
+                <div className="col-span-6 text-xl">{ isMusic() ? `${author}` : `${date}, ${location}, ${duration}` }</div>
+                <img className="row-span-6 col-span-6 sm:col-span-2 mx-auto h-full rounded" src={img} alt={img}/>
+                <div className="row-span-4 col-span-6 sm:col-span-4 mx-2 text-base sm:text-lg leading-normal overflow-y-auto">{description}</div>
             </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default Popup
