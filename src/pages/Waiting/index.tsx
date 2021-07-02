5378b5e979a186b1a01ed5467807f2f1023a3113
bastiengrignon <basgrignon@gmail.com>
2021-07-02 17:38:53 +0200

    :iphone: Update text & image size for screen

--- src/pages/Waiting/index.tsx
+++ src/pages/Waiting/index.tsx
@@ -5,8 +5,8 @@ const Waiting: React.FC = () => {
     return (
         <div className="bg-test-green h-screen w-full flex flex-col items-center justify-center text-white space-y-10">
             <img src={`${process.env.PUBLIC_URL}/img/test_logo11.png`} alt="Logo Mazette"
-                className="object-contain h-auto w-1/4"/>
-            <div className="text-5xl animate-pulse">Le site n&apos;est pas encore disponible </div>
+                className="object-contain h-auto w-3/4 sm:w-1/2 lg:w-1/2"/>
+            <div className="text-xl sm:text-2xl md:text-5xl animate-pulse">Le site n&apos;est pas encore disponible </div>
         </div>
     )
 }
