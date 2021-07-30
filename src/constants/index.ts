6c89469aef2c3869d4e957ba3b6815851ec7cb6b
bastiengrignon <basgrignon@gmail.com>
2021-07-30 10:42:04 +0200

    :bento: Add link of new artist

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -82,6 +82,8 @@ export const externalLinks = {
     },
     music: {
         oulitsa: "https://youtu.be/9YVuyx6SoVA",
+        elayiis: "https://www.youtube.com/channel/UCtKeqrDiSDL9pH5lsOZuYWw",
+        elayiis_ig: "https://www.instagram.com/elayiis/",
         wugo: "https://youtu.be/wc3LAkClt0k"
     },
     films: {
