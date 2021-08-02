48ba009e2df792bc64dd0ceb9d21215d5643691a
bastiengrignon <basgrignon@gmail.com>
2021-08-02 23:35:39 +0200

    :sparkles: Finish logIn API request

--- src/services/admin/authentication/authentication.service.ts
+++ src/services/admin/authentication/authentication.service.ts
@@ -1,6 +1,4 @@
 import axios, { AxiosResponse } from "axios"
-import { tokenKey } from "./authentication.constants"
-import { Cookies } from "../storage/cookie.services"
 
 export interface ISignInForm {
     username: string
@@ -8,14 +6,5 @@ export interface ISignInForm {
 }
 
 export class AuthenticationService {
-    static logInAsync = async (signInForm: ISignInForm): Promise<AxiosResponse> => {
-        const authResponse = await axios.post("/auth", signInForm)
-        const token = authResponse.data
-        Cookies.set(tokenKey, token)
-        axios.defaults.headers = { Cookies: `${tokenKey}=${Cookies.get(tokenKey)}` }
-
-        console.log(authResponse)
-
-        return authResponse
-    }
+    static logInAsync = async (signInForm: ISignInForm): Promise<AxiosResponse> => await axios.post("/auth", signInForm).then(r => r.data)
 }
