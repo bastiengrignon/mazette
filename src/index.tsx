119bc054ab3b5a519d9ef3d00c31bc58512e48f7
bastiengrignon <basgrignon@gmail.com>
2021-08-02 15:30:10 +0200

    :recycle: Refactor App

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import "antd/dist/antd.css"
 import "./index.css"
 
 import Home from "./pages/Home"
+
 const Footer = loadable(() => import("./components/Footer"))
 const Navbar = loadable(() => import("./components/Navbar"))
 const Information = loadable(() => import("./pages/Information"))
@@ -20,43 +21,37 @@ const Dashboard = loadable(() => import("./pages/admin/Dashboard"))
 const DashboardMovie = loadable(() => import("./pages/admin/DashboardMovie"))
 const DashboardMusic = loadable(() => import("./pages/admin/DashboardMusic"))
 
-const subDomain = window.location.host.split(".")[0]
+const App: React.FC = () => (
+    <div className="min-h-full flex flex-col justify-between">
+        <Router>
+            { isAdminRoutes() ? null : <Navbar/> }
+            {
+                isAdminRoutes()
+                    ?
+                    <Switch>
+                        <Route path={ RouterUrl.adminMovie } component={ DashboardMovie }/>
+                        <Route path={ RouterUrl.adminMusic } component={ DashboardMusic }/>
 
+                        <Route path={ RouterUrl.home } component={ Dashboard }/>
+                    </Switch>
+                    :
+                    <Switch>
 
-const App: React.FC = () => {
+                        <Route path={ RouterUrl.programmation } component={ Programmation }/>
+                        <Route path={ RouterUrl.association } component={ Association }/>
+                        <Route path={ RouterUrl.information } component={ Information }/>
+                        <Route path={ RouterUrl.mention } component={ LegalMention }/>
+                        <Route path={ RouterUrl.passSanitaire } component={ SanitaryPass }/>
 
-    return (
-        <div className="min-h-full flex flex-col justify-between">
-            <Router>
-                { isAdminRoutes() ? null : <Navbar/> }
-                {
-                    isAdminRoutes()
-                        ?
-                        <Switch>
-                            <Route path={ RouterUrl.adminMovie } component={ DashboardMovie }/>
-                            <Route path={ RouterUrl.adminMusic } component={ DashboardMusic }/>
+                        <Route path={ RouterUrl.home } component={ Home }/>
+                    </Switch>
+            }
+            { isAdminRoutes() ? null : <Footer/> }
+        </Router>
+    </div>
+)
 
-                            <Route path={ RouterUrl.home } component={ Dashboard }/>
-                        </Switch>
-                        :
-                        <Switch>
-
-                            <Route path={ RouterUrl.programmation } component={ Programmation }/>
-                            <Route path={ RouterUrl.association } component={ Association }/>
-                            <Route path={ RouterUrl.information } component={ Information }/>
-                            <Route path={ RouterUrl.mention } component={ LegalMention }/>
-                            <Route path={ RouterUrl.passSanitaire } component={ SanitaryPass }/>
-
-                            <Route path={ RouterUrl.home } component={ Home }/>
-                        </Switch>
-                }
-                { isAdminRoutes() ? null : <Footer/> }
-            </Router>
-        </div>
-    )
-}
-
-const isAdminRoutes = (): boolean => subDomain === adminSubdomain;
+const isAdminRoutes = (): boolean => window.location.host.split(".")[0] === adminSubdomain;
 
 (() => {
     axios.defaults.baseURL = process.env.REACT_APP_API_URL
