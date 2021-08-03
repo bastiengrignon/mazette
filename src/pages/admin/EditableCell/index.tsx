52ee813821b47e1a1385784ebded00023ffbada9
bastiengrignon <basgrignon@gmail.com>
2021-08-03 19:13:12 +0200

    :sparkles: Handle inputType props

--- src/pages/admin/EditableCell/index.tsx
+++ src/pages/admin/EditableCell/index.tsx
@@ -5,6 +5,7 @@ interface EditableCellProps extends React.HTMLAttributes<HTMLElement> {
     editing: boolean
     dataIndex: string
     title: string
+    inputType: "text" | "textarea";
     children: React.ReactNode
 }
 
@@ -12,25 +13,30 @@ const EditableCell: React.FC<EditableCellProps> = ({
     editing,
     dataIndex,
     title,
+    inputType,
     children,
     ...restProps
-}) => (
-    <td { ...restProps }>
-        { editing ?
-            <Form.Item
-                name={ dataIndex }
-                style={ { margin: 0 } }
-                rules={ [
-                    {
-                        required: true,
-                        message: `Please Input ${ title }!`,
-                    },
-                ] }
-            >
-                <Input/>
-            </Form.Item> :
-            children
-        }
-    </td>
-)
+}) => {
+    const inputNode = inputType === "textarea" ? <Input.TextArea /> : <Input />
+
+    return (
+        <td { ...restProps }>
+            { editing ?
+                <Form.Item
+                    name={ dataIndex }
+                    style={ { margin: 0 } }
+                    rules={ [
+                        {
+                            required: true,
+                            message: `Please Input ${ title }!`,
+                        },
+                    ] }
+                >
+                    { inputNode }
+                </Form.Item> :
+                children
+            }
+        </td>
+    )
+}
 export default EditableCell
