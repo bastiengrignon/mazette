c9fffc6e66ef494f1a5ec41e79947d38f0680005
bastiengrignon <basgrignon@gmail.com>
2021-08-03 12:08:50 +0200

    :sparkles: Add create Movie API service

--- src/services/admin/movie/movie.service.ts
+++ src/services/admin/movie/movie.service.ts
@@ -2,11 +2,9 @@ import axios from "axios"
 import { IMovie } from "./movie.interface"
 
 export class MovieService {
-    static getAll = async (): Promise<IMovie[]> => {
-        return await axios.get("/movie").then(r => r.data)
-    }
+    static getAll = async (): Promise<IMovie[]> => await axios.get("/movie").then(r => r.data)
 
-    static update = async (id: number, updatedMovie: IMovie): Promise<IMovie> => {
-        return await axios.put(`/movie/${id}`, updatedMovie).then(r => r.data)
-    }
+    static create = async (movie: IMovie): Promise<IMovie> => await axios.post("/movie", movie).then(r => r.data)
+
+    static update = async (id: number, updatedMovie: IMovie): Promise<IMovie> => await axios.put(`/movie/${ id }`, updatedMovie).then(r => r.data)
 }
