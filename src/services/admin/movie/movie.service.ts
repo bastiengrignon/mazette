05a4c8c84868ee25877be1dfe82b38b6335c3f47
bastiengrignon <basgrignon@gmail.com>
2021-08-03 21:29:19 +0200

    :art: Format dates of movie

--- src/services/admin/movie/movie.service.ts
+++ src/services/admin/movie/movie.service.ts
@@ -4,7 +4,19 @@ import { IMovie } from "./movie.interface"
 export class MovieService {
     static getAll = async (): Promise<IMovie[]> => await axios.get("/movie").then(r => r.data)
 
-    static create = async (movie: IMovie): Promise<IMovie> => await axios.post("/movie", movie).then(r => r.data)
+    static create = async (movie: IMovie, file: string): Promise<IMovie> => {
+        const tmpMovie = { ...MovieService.formatMovieDates(movie), imgThumbnail: file }
+        return await axios.post("/movie", tmpMovie).then(r => r.data)
+    }
 
     static update = async (id: number, updatedMovie: IMovie): Promise<IMovie> => await axios.put(`/movie/${ id }`, updatedMovie).then(r => r.data)
+
+    private static formatMovieDates = (movie: IMovie): IMovie => {
+        return {
+            ...movie,
+            date: new Date(movie.date).getUTCFullYear().toString(),
+            publicationDate: new Date(movie.publicationDate).getUTCDate().toString(),
+            duration: `${ new Date(movie.duration).getUTCMinutes() }min ${ new Date(movie.duration).getUTCSeconds() }s`
+        }
+    }
 }
