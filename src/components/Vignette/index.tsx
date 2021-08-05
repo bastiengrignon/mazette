b7b745c8eeceb31ca4fc5c91dbf8415230005e5d
bastiengrignon <basgrignon@gmail.com>
2021-08-05 02:51:27 +0200

    :lipstick: Use antd Modal instead of custom Popup

--- src/components/Vignette/index.tsx
+++ src/components/Vignette/index.tsx
@@ -1,58 +1,79 @@
 import React, { useState } from "react"
-import Popup from "../Popup"
 import { AdvancedImage } from "@cloudinary/react"
 import { cloudinary } from "../../index"
-import { Skeleton } from "antd"
+import { Modal, Skeleton } from "antd"
 import { IMusic } from "../../services/admin/music/music.interface"
 import { IMovie } from "../../services/admin/movie/movie.interface"
+import { fill } from "@cloudinary/base/actions/resize"
 
 const vignetteCSS = "w-11/12 sm:w-7/8 md:w-3/4 h-auto mx-auto cursor-pointer transform transition duration-300 hover:scale-110"
 
 interface VignetteProps<MediaType extends IMovie | IMusic> {
     type: "music" | "movie"
-    //eslint-disable-next-line
     properties: MediaType
     loading: boolean
 }
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const Vignette: React.FC<VignetteProps<any>> = (props) => {
+const Vignette: React.FC<VignetteProps<any>> = ({ properties , ...props}) => {
     const [visibility, setVisibility] = useState<boolean>(false)
 
-    return props.type === "music" ?
-        <Skeleton avatar={ true } active={ true } paragraph={{ rows: 6 }} loading={ props.loading }>
-            <div className={ `relative ${ vignetteCSS }` } onClick={ () => setVisibility(true) }>
-                <AdvancedImage cldImg={ cloudinary.image(`/${props.properties.image}`) } alt={ props.properties.name }/>
-                <div className="fixed top-0 h-full w-full opacity-0 hover:opacity-100">
-                    <div className="flex justify-center items-center h-full">
+    const isMusic = (): boolean => (properties as IMusic).name !== undefined
+    const publicImgId = (): string => isMusic() ? properties.image : properties.imgThumbnail
+    const altImgName = (): string => isMusic() ? properties.name : properties.title
+
+    const formattedModalTitle = (): React.ReactNode => (
+        <div className="font-semibold uppercase text-xl sm:text-2xl">
+            {
+                isMusic() ? <span>{ properties.name }</span> :
+                    <span>{ properties.title }
                         <span
-                            className="w-full font-medium bg-green text-white text-center text-base sm:text-2xl md:text-3xl lg:text-5xl py-2">
-                            { props.properties.name }
-                        </span>
+                            className="text-sm sm:text-base italic lowercase"> de { properties.author }</span>
+                    </span>
+            }
+        </div>
+    )
+
+    return (
+        <>
+            {
+                props.type === "music" ?
+                    <Skeleton avatar={ true } active={ true } paragraph={{ rows: 6 }} loading={ props.loading }>
+                        <div className={ `relative ${ vignetteCSS }` } onClick={ () => setVisibility(true) }>
+                            <AdvancedImage cldImg={ cloudinary.image(`/${properties.image}`) } alt={ properties.name }/>
+                            <div className="fixed top-0 h-full w-full opacity-0 hover:opacity-100">
+                                <div className="flex justify-center items-center h-full">
+                                    <span className="w-full font-medium bg-green text-white text-center text-base sm:text-2xl md:text-3xl lg:text-5xl py-2">
+                                        { properties.name }
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </Skeleton>
+                    :
+                    <Skeleton avatar={ true } active={ true } paragraph={{ rows: 6 }} loading={ props.loading }>
+                        <div className={ `relative ${ vignetteCSS }` } onClick={ () => setVisibility(true) }>
+                            <AdvancedImage cldImg={ cloudinary.image(`/${properties.imgThumbnail}`) }
+                                alt={ `${ properties.title } ${ properties.author }` }
+                                className="w-full h-full"/>
+                        </div>
+                    </Skeleton>
+            }
+            <Modal visible={ visibility } onCancel={ () => setVisibility(false)} footer={ null }
+                title={ formattedModalTitle() } centered={ true }>
+                <div className="text-xl">
+                    { isMusic() ? `${ properties.type }` : `${ properties.location }, ${ properties.duration } (${ properties.date })` }
+                </div>
+                <div className="grid grid-cols-6 grid-rows-1">
+                    <div className="col-span-6 sm:col-span-6 md:col-span-2 flex justify-center my-2 md:my-0">
+                        <AdvancedImage cldImg={ cloudinary.image(publicImgId()).resize(fill().width(250).height(250)) } alt={ altImgName() } className="rounded"/>
+                    </div>
+                    <div className="col-span-6 sm:col-span-6 md:col-span-4 mx-2 text-base sm:text-lg overflow-y-auto">
+                        { properties.description }
                     </div>
                 </div>
-            </div>
-            { visibility &&
-            <Popup name={ props.properties.name } author={ props.properties.type }
-                description={ props.properties.description } date={ "" } location={ "" }
-                duration={ "" } visibility={ setVisibility } img={ props.properties.image }/>
-            }
-
-        </Skeleton>
-        :
-        <Skeleton avatar={ true } active={ true } paragraph={{ rows: 6 }} loading={ props.loading }>
-            <div className={ `relative ${ vignetteCSS }` } onClick={ () => setVisibility(true) }>
-                <AdvancedImage cldImg={ cloudinary.image(`/${props.properties.imgThumbnail}`) }
-                    alt={ `${ props.properties.title } ${ props.properties.author }` }
-                    className="w-full h-full"/>
-            </div>
-            { visibility &&
-            <Popup name={ props.properties.title } author={ props.properties.author }
-                description={ props.properties.description } date={ props.properties.date }
-                location={ props.properties.location } duration={ props.properties.duration }
-                visibility={ setVisibility }
-                img={ props.properties.imgThumbnail }/>
-            }
-        </Skeleton>
+            </Modal>
+        </>
+    )
 }
 
 export default Vignette
