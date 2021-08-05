b9c8f6bb97df1e9b6d6155c982ac31fdf12cc682
bastiengrignon <basgrignon@gmail.com>
2021-08-05 16:59:05 +0200

    :truck: Move connected user check

--- src/services/admin/authentication/authentication.service.ts
+++ src/services/admin/authentication/authentication.service.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from "axios"
+import { authToken, Storage } from "../storage/storage.services"
 
 export interface ISignInForm {
     username: string
@@ -7,4 +8,6 @@ export interface ISignInForm {
 
 export class AuthenticationService {
     static logInAsync = async (signInForm: ISignInForm): Promise<AxiosResponse> => await axios.post("/auth", signInForm).then(r => r.data)
+
+    static connectedUserCookie = (): boolean => Storage.get(authToken) === "true"
 }
