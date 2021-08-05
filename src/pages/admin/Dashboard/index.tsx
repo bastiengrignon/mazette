636d0ed67cadf4eb83b4c10463f458b784608d03
bastiengrignon <basgrignon@gmail.com>
2021-08-05 17:00:45 +0200

    :truck: Move login modal into Navigation component

--- src/pages/admin/Dashboard/index.tsx
+++ src/pages/admin/Dashboard/index.tsx
@@ -1,42 +1,17 @@
 import React, { useEffect, useState } from "react"
 import Navigation from "../Navigation"
 import {
-    Button,
     Card,
-    Form,
-    Input,
     List,
-    message,
-    Modal,
     Skeleton,
     Typography
 } from "antd"
-import { useForm } from "antd/es/form/Form"
-import { AuthenticationService } from "../../../services/admin/authentication/authentication.service"
-import { authToken, Storage } from "../../../services/admin/storage/storage.services"
 import { IText } from "../../../services/admin/text/text.interface"
 import { TextService } from "../../../services/admin/text/text.service"
 
 const Dashboard: React.FC = () => {
-    const connectedUserCookie = (): boolean => Storage.get(authToken) === "true"
-
-    const [isModalVisible, setIsModalVisible] = useState(!connectedUserCookie())
     const [isTextLoading, setIsTextLoading] = useState<boolean>(false)
-    const [loginForm] = useForm()
     const [texts, setTexts] = useState<IText[]>([])
-
-    const handleLogin = () => {
-        loginForm.validateFields()
-            .then(values => {
-                AuthenticationService
-                    .logInAsync(values)
-                    .then(() => Storage.set(authToken, "true"))
-                    .catch(() => Storage.delete(authToken))
-                    .finally(() => loginForm.resetFields())
-                setIsModalVisible(false)
-            })
-            .catch(info => message.warn("Validation failed: ", info))
-    }
 
     useEffect(() => {
         setIsTextLoading(true)
@@ -56,34 +31,14 @@ const Dashboard: React.FC = () => {
 
     return (
         <Navigation>
-            {
-                !isModalVisible &&
-                    <div className="grid grid-cols-6">
-                        <Card bordered={ false } className="rounded-lg col-span-3">
-                            <p className="text-xl mb-2">Textes d’introduction : </p>
-                            <Skeleton avatar={ true } active={ true } loading={ isTextLoading } >
-                                <List dataSource={ texts } renderItem={ renderListTextItem }/>
-                            </Skeleton>
-                        </Card>
-                    </div>
-            }
-            <Modal title="Admin Login" visible={ isModalVisible } footer={ null } closable={ false }>
-                <Form name="login_form" form={ loginForm } onFinish={ handleLogin } initialValues={ { remember: true } }>
-                    <Form.Item label="Nom d'utilisateur" name="username" rules={ [{ required: true, message: "Veuillez entrer votre nom d'utilisateur!" }] }>
-                        <Input/>
-                    </Form.Item>
-
-                    <Form.Item label="Mot de passe" name="password" rules={ [{ required: true, message: "Veuillez entrer votre mot de passe !" }] }>
-                        <Input.Password/>
-                    </Form.Item>
-
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" className="w-full">
-                            Connexion
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </Modal>
+            <div className="grid grid-cols-6">
+                <Card bordered={ false } className="rounded-lg col-span-3">
+                    <p className="text-xl mb-2">Textes d’introduction : </p>
+                    <Skeleton avatar={ true } active={ true } loading={ isTextLoading } >
+                        <List dataSource={ texts } renderItem={ renderListTextItem }/>
+                    </Skeleton>
+                </Card>
+            </div>
         </Navigation>
     )
 }
--- src/pages/admin/Navigation/index.tsx
+++ src/pages/admin/Navigation/index.tsx
@@ -1,18 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 import {
     DashboardOutlined,
     FireFilled,
     PlayCircleOutlined,
     TeamOutlined,
-    VideoCameraOutlined
+    VideoCameraOutlined,
+    LogoutOutlined
 } from "@ant-design/icons"
 import { Link, useLocation } from "react-router-dom"
 import { programmationTitle, RouterUrl } from "../../../constants"
+import { authToken, Storage } from "../../../services/admin/storage/storage.services"
+import { AuthenticationService } from "../../../services/admin/authentication/authentication.service"
+import { Button, Form, Input, message, Modal } from "antd"
+import { useForm } from "antd/es/form/Form"
 
 const activatedClassCSS = "flex items-center mt-4 py-2 px-6 bg-gray-700 bg-opacity-25 text-gray-100"
 const deactivatedClassCSS = "flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
 
 const Navigation: React.FC = ({ children }) => {
+    const [isModalVisible, setIsModalVisible] = useState(!AuthenticationService.connectedUserCookie())
+    const [loginForm] = useForm()
     const location = useLocation()
 
     const adminTitleFromPathname = (url: string): string => {
@@ -29,6 +36,27 @@ const Navigation: React.FC = ({ children }) => {
         else return <DashboardOutlined />
     }
 
+    const logout = () => {
+        window.location.href.replace("admin.", "")
+        Storage.delete(authToken)
+        window.location.reload()
+    }
+
+    const handleLogin = () => {
+        loginForm.validateFields()
+            .then(values => {
+                AuthenticationService
+                    .logInAsync(values)
+                    .then(() => Storage.set(authToken, "true"))
+                    .catch(() => Storage.delete(authToken))
+                    .finally(() => loginForm.resetFields())
+                setIsModalVisible(false)
+            })
+            .catch(info => message.warn("Validation failed: ", info))
+    }
+
+
+
     return (
         <div className="grid grid-cols-6 h-screen">
             <div className="col-span-2 md:col-span-1 bg-gray-900">
@@ -43,17 +71,23 @@ const Navigation: React.FC = ({ children }) => {
                         <DashboardOutlined />
                         <span className="mx-3 capitalize">Dashboard</span>
                     </Link>
-                    {
-                        Object.keys(RouterUrl).filter(key => key.match(/^admin.*$/g)).map((url, key) => {
-                            const modifiedUrl = `/${ url.split("admin")[1].toString().toLowerCase() }`
-                            return (
-                                <Link key={ key } to={ modifiedUrl } className={ location.pathname === modifiedUrl ? activatedClassCSS : deactivatedClassCSS }>
-                                    { adminTabIcon(modifiedUrl) }
-                                    <span className="mx-3 capitalize"> { adminTitleFromPathname(modifiedUrl) }</span>
-                                </Link>
-                            )
-                        })
-                    }
+                    <div>
+                        {
+                            Object.keys(RouterUrl).filter(key => key.match(/^admin.*$/g)).map((url, key) => {
+                                const modifiedUrl = `/${ url.split("admin")[1].toString().toLowerCase() }`
+                                return (
+                                    <Link key={ key } to={ modifiedUrl } className={ location.pathname === modifiedUrl ? activatedClassCSS : deactivatedClassCSS }>
+                                        { adminTabIcon(modifiedUrl) }
+                                        <span className="mx-3 capitalize"> { adminTitleFromPathname(modifiedUrl) }</span>
+                                    </Link>
+                                )
+                            })
+                        }
+                    </div>
+                    <Link to={ RouterUrl.home } onClick={ logout } className={ `${deactivatedClassCSS} mt-24` }>
+                        <LogoutOutlined />
+                        <span className="mx-3 capitalize">Déconnexion</span>
+                    </Link>
                 </nav>
             </div>
             <div className="col-span-4 md:col-span-5 bg-gray-200">
@@ -61,9 +95,26 @@ const Navigation: React.FC = ({ children }) => {
                     { adminTitleFromPathname(location.pathname) }
                 </header>
                 <main className="overflow-x-hidden overflow-y-auto px-4 mt-4">
-                    { children }
+                    { !isModalVisible && children }
                 </main>
             </div>
+            <Modal title="Admin Login" visible={ isModalVisible } footer={ null } closable={ false }>
+                <Form name="login_form" form={ loginForm } onFinish={ handleLogin } initialValues={ { remember: true } }>
+                    <Form.Item label="Nom d'utilisateur" name="username" rules={ [{ required: true, message: "Veuillez entrer votre nom d'utilisateur!" }] }>
+                        <Input/>
+                    </Form.Item>
+
+                    <Form.Item label="Mot de passe" name="password" rules={ [{ required: true, message: "Veuillez entrer votre mot de passe !" }] }>
+                        <Input.Password/>
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" className="w-full">
+                            Connexion
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Modal>
         </div>
     )
 }
