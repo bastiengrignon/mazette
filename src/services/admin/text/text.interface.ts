844495a008db281d40e29d2dff5a8907532e03ae
bastiengrignon <basgrignon@gmail.com>
2021-08-08 13:21:17 +0200

    :sparkles: Add TextType enum

--- src/services/admin/text/text.interface.ts
+++ src/services/admin/text/text.interface.ts
@@ -1,6 +1,18 @@
 export interface IText {
     id: number
     text: string
+    type: TextType
     updatedAt?: Date
     createdAt?: Date
+}
+
+export enum TextType {
+    music = "music",
+    movie = "movie",
+    contest = "contest",
+    association = "association",
+    team = "team",
+    adhere = "adhere",
+    food = "food",
+    journey = "journey"
 }
