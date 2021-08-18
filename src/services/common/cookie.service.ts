76694712aeaa372180aa51389f1e8f26f4ec9975
bastiengrignon <basgrignon@gmail.com>
2021-08-18 11:42:36 +0200

    :art: Improve cookie service

--- src/services/common/cookie.service.ts
+++ src/services/common/cookie.service.ts
@@ -1,17 +1,22 @@
+import { adminSubdomain } from "../../constants"
 
 export class CookieService {
     static cookieName = "allow-cookies"
 
-    static cookieExists = (): boolean => CookieService.get(CookieService.cookieName) !== ""
     static isCookiesAllowed = (): boolean => {
         if (!CookieService.cookieExists()) return false
         return CookieService.get(CookieService.cookieName) === "true"
     }
+    static cookieNeedsToBeDisplayed = (): boolean => {
+        const isLocalhost = window.location.href.includes("localhost")
+        const isAdminPanel = window.location.host.split(".")[0].includes(adminSubdomain)
+        const isCookieExists = CookieService.cookieExists()
+        return isLocalhost && !isAdminPanel && !isCookieExists
+    }
 
     static get = (key: string): string => {
         const match = document.cookie.match(new RegExp("(^| )" + key + "=([^;]+)"))
-        if (match) return match[2]
-        return ""
+        return match ? match[2] : ""
     }
 
     static set = (key: string, value: string): void => {
@@ -23,4 +28,6 @@ export class CookieService {
     static delete = (key: string): void => {
         document.cookie = `${ key }=; max-age=0;`
     }
+
+    private static cookieExists = (): boolean => CookieService.get(CookieService.cookieName) !== ""
 }
