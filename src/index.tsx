e077ecc5d4c71f6fe03b3c03bc71412a953c643a
bastiengrignon <basgrignon@gmail.com>
2021-10-21 22:10:38 +0200

    :sparkles: Add Sentry config

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import { ConfigProvider } from 'antd'
 import axios from 'axios'
 import frFR from 'antd/lib/locale/fr_FR'
 
+import * as Sentry from '@sentry/react'
 import { Cloudinary } from '@cloudinary/base'
+import { Integrations } from '@sentry/tracing'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
@@ -13,6 +15,14 @@ import Routes from './components/Routes'
 import { adminSubdomain } from './constants'
 import 'antd/dist/antd.css'
 import './index.css'
+
+Sentry.init({
+    dsn         : process.env.REACT_APP_SENTRY_DSN,
+    integrations: [new Integrations.BrowserTracing()],
+    // We recommend adjusting this value in production, or using tracesSampler for finer control
+    tracesSampleRate: 0.5
+})
+
 
 const App: React.FC = () => (
     <div className="min-h-full flex flex-col justify-between">
