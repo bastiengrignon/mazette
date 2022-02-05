e7096f2a3f5ed7438072752099b1ce46d05703b7
bastiengrignon <basgrignon@gmail.com>
2022-02-05 11:30:39 +0100

    Update movie & music to match right festival date

--- src/pages/Programmation/index.tsx
+++ src/pages/Programmation/index.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react'
 import { FESTIVAL_ID, programmationTitle } from '../../constants'
 import { FestivalService, IFestival } from '../../services/admin/festival'
 import { IMovie, IMusic, MovieService, MusicService, TextType } from '../../services'
-import { formatDates, getDatesBetween, numericDateRegexp } from '../../lib/date'
+import { datesMatched, formatDates, getDatesBetween } from '../../lib/date'
 
 const Anchor = loadable(() => import('../../components/Anchor'))
 const Vignette = loadable(() => import('../../components/Vignette'))
@@ -32,11 +32,7 @@ const Programmation: React.FC = () => {
         MovieService.getAll().then(setMovies).finally(() => setIsMovieLoading(false))
     }, [])
 
-    const getFestivalDate = (festival: IFestival) => {
-        const { startDate, endDate } = festival
-        const festivalDates = getDatesBetween(new Date(startDate), new Date(endDate), true)
-        return formatDates(festivalDates)
-    }
+    const getFestivalDate = (festival: IFestival) => getDatesBetween(new Date(festival.startDate), new Date(festival.endDate), true)
 
     return (
         <div className="flex flex-col z-10 page-content">
@@ -45,23 +41,19 @@ const Programmation: React.FC = () => {
                 <FormattedText textType={ TextType.music }/>
             </div>
             {
-                festival.id && getFestivalDate(festival).map((date, index) => {
-                    const day = date.match(numericDateRegexp)?.[0]
-                    return (
-                        <div key={index}>
-                            <p className={ dateCSS }>{ date }</p>
+                festival.id && getFestivalDate(festival).map((date, index) =>
+                    (
+                        <div key={ index }>
+                            <p className={ dateCSS }>{ formatDates(date) }</p>
                             <div className="grid grid-cols-2 gap-2 sm:gap-10">
                                 {
-
-                                    musics.filter(music => music.publicationDate.getDate() === Number(day)).map((music, key) => (
+                                    musics.filter(music => datesMatched(music.publicationDate, date)).map((music, key) => (
                                         <Vignette key={ key } type="music" properties={ music } loading={ isMusicLoading }/>
                                     ))
                                 }
                             </div>
                         </div>
-                    )
-
-                })
+                    ))
             }
 
             <Anchor id={ programmationTitle.films } className={ titleCSS }/>
@@ -69,22 +61,19 @@ const Programmation: React.FC = () => {
                 <FormattedText textType={ TextType.movie }/>
             </div>
             {
-                festival.id && getFestivalDate(festival).map((date, index) => {
-                    const day = date.match(numericDateRegexp)?.[0]
-                    return (
-                        <div key={index}>
-                            <p className={ dateCSS }>{ date }</p>
+                festival.id && getFestivalDate(festival).map((date, index) =>
+                    (
+                        <div key={ index }>
+                            <p className={ dateCSS }>{ formatDates(date) }</p>
                             <div className="grid grid-cols-2 gap-2 sm:gap-10">
                                 {
-                                    movies.filter(film => film.publicationDate === day).map((film, key) => (
+                                    movies.filter(film => datesMatched(film.publicationDate, date)).map((film, key) => (
                                         <Vignette key={ key } type="movie" properties={ film } loading={ isMovieLoading }/>
                                     ))
                                 }
                             </div>
                         </div>
-                    )
-
-                })
+                    ))
             }
             <Anchor id={ programmationTitle.concours } className={ titleCSS }/>
             <div className={ subtitleCSS }>
