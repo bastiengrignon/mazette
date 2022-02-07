452c50b35990b152a0ea0991c6c805aa1e951ef7
bastiengrignon <basgrignon@gmail.com>
2022-02-07 20:17:15 +0100

    Update commonService function for table column

--- src/services/admin/common/common.service.ts
+++ src/services/admin/common/common.service.ts
@@ -1,23 +1,22 @@
-import { IPartner } from '../partner'
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 export class CommonService {
-    // eslint-disable-next-line
-    static mergedColumns = (columns: any, isEditing: (record: any) => boolean): [] => {
-        return columns.map((col) => {
+    static mergedColumns = (columns: any[], isEditing: (record: any) => boolean): any[] =>
+        columns.map((col) => {
             if (!col.editable) return col
             return {
                 ...col,
-                onCell: (record: IPartner) => ({
+                onCell: (record: any) => ({
                     record,
-                    inputType: col.inputType || 'text',
-                    dataIndex: col.dataIndex,
-                    title    : col.title,
-                    required : col.dataIndex !== 'videoLink',
-                    editing  : isEditing(record)
-                }),
+                    inputType   : col.inputType || 'text',
+                    dataIndex   : col.dataIndex,
+                    title       : col.title,
+                    required    : col.dataIndex !== 'videoLink',
+                    editing     : isEditing(record),
+                    currentValue: record[col.dataIndex]
+                })
             }
         })
-    }
 
     static capitalize = (string: string): string => string.charAt(0).toUpperCase() + string.slice(1)
 }
